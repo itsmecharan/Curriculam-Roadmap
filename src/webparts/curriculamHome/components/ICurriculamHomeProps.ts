51ea7941b185d8d9f51787bab5493e15468f9641
Charan Paladugu <v-cpaladugu@microsoft.com>
2023-09-28 13:10:49 +0530

    Created SPFx Webpart

---
+++ src/webparts/curriculamHome/components/ICurriculamHomeProps.ts
@@ -0,0 +1,7 @@
+export interface ICurriculamHomeProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/curriculamHome/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ICurriculamHomeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'CurriculamHomeWebPartStrings' {
+  const strings: ICurriculamHomeWebPartStrings;
+  export = strings;
+}
